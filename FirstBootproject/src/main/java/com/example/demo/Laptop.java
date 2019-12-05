3039c0bae1e910947fae87a04bfa476473606cbd
bharathadpad97 <58290681+bharathadpad97@users.noreply.github.com>
2019-12-05 17:51:28 +0530

    SpringBoot-1

---
+++ FirstBootproject/src/main/java/com/example/demo/Laptop.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Laptop {
+	
+	public void laptopmethod()
+	{
+		System.out.println("Laptop method printed");
+	}
+
+}
